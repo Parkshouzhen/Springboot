93dd0d99af2491e23217bde8cdb89190608e08e2
Parkshouzhen <shouzhenn@gmail.com>
2021-01-22 00:03:51 +0900

    jpa

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,45 @@
 package hello.hellospring;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+	
+	private  EntityManager em;
+	
+	@Autowired
+	public SpringConfig(EntityManager em) {
+		this.em = em;
+	}
 
-	@Bean // 스프링 빈 등록한다
+//	private final DataSource dataSource;
+//
+//	public SpringConfig(DataSource dataSource) {
+//		this.dataSource = dataSource;
+//	}
+
+	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
-	
+
 	@Bean
-	public MemberRepository memberRepository() { //MemberRepository: 인터페이스
-		return new MemoryMemberRepository(); //MemoryMemberRepository : 구현체
+	public MemberRepository memberRepository() {
+		// return new MemoryMemberRepository();
+		// return new JdbcMemberRepository(dataSource);
+		// return new JdbcTemplateMemberRepository(dataSource);
+		return new JpaMemberRepository(em);
 	}
 }
